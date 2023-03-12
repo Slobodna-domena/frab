f9cd31467d28be8f617034b3a2415a26ba0ead27
StjepanSD <vrljicak.stjepan@gmail.com>
2023-03-12 18:08:04 +0100

    Update send_bulk_mail_job.rb

--- app/jobs/send_bulk_mail_job.rb
+++ app/jobs/send_bulk_mail_job.rb
@@ -30,7 +30,7 @@ class SendBulkMailJob
       destination_event_people = destination
     end
     
-    ep_ids = (1..295).to_a + [376,318]
+    ep_ids = (1..348).to_a + [376,318]
     destination_event_people = destination_event_people.where("event_id not in (?) and event_role <> 'submitter'", ep_ids)
     destination_event_people.each do |dep|
       UserMailer.bulk_mail_multiple_roles(EventPerson.where(id: dep.id), template).deliver_now
