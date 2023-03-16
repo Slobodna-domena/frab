31f1cbbac1607f3bf7bce835623cc9cd57f5a7b1
Stjepan Vrlji <stjepanvrlji@sdlaptop1>
2023-03-16 13:50:31 +0100

    test without user 9

--- app/jobs/send_bulk_mail_job.rb
+++ app/jobs/send_bulk_mail_job.rb
@@ -33,6 +33,7 @@ class SendBulkMailJob
     #ep_ids = (1..347).to_a + [376,318]
     #destination_event_people = destination_event_people.where("event_id not in (?) and event_role <> 'submitter'", ep_ids)
     destination_event_people.pluck(:person_id).uniq.each do |p_id|
+      next if p_id == 9
       #UserMailer.bulk_mail_multiple_roles(destination_event_people.where(person_id: p_id), template).deliver_now
       p=Person.find(p_id)
       Rails.logger.info "Mail template #{template.name} delivered to #{p.first_name} #{p.last_name} (#{p.email})"
